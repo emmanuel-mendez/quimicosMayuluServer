a03312920e4c11a3bb66182ea3c66568bcaadc16
emmanelos <emmanelos@protonmail.com>
2020-09-25 23:40:43 -0400

    fixed PORT

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const resolvers = require('./graphql/resolvers')
 const { MONGODB } =require('./config/database')
 const checkAuth = require('./util/checkAuth')
 
+const PORT = process.env.PORT || '5000'
 
 const server = new ApolloServer({
     typeDefs,
@@ -18,9 +19,8 @@ mongoose.connect(MONGODB, {
     useUnifiedTopology: true 
 })
     .then(() =>{
-        app.listen(process.env.PORT || 5000, function(){
-            console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-        });
+        console.log('MongoDb connected')
+        return server.set(`port`, process.env.PORT || 3000)
     })
     .then(res => {
         console.log(`Server is running at ${res.url}`)
